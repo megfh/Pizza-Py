b4d3d6a66f591b586a0da11111651a7dfda78d2b
Meghan Hannon <mhannon4@uwo.ca>
2019-07-10 22:01:58 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,73 @@
+# FREE PIZZA 
+
+Are you a #LdnOnt native? Did you miss your free name day at Stobies? Don't let that happen again! 
+This python script that will check Stobies twitter and text you if it's your day for free pizza 🍕
+
+## Getting Started 
+These instructions will get you a copy of the project up and running on your local machine for development and testing purposes.
+
+### Prerequisites 
+* **Python 3.5+**
+* **Twitter Developer Account + App created**
+* **Twilio Account** 
+* **config**.py 
+
+### Installing 
+Clone the repository 
+```shell
+$ git clone https://github.com/megfh/Pizza-Py.git
+```
+
+Install required libraries 
+```shell
+$ pip3 install -r requirements.txt 
+```
+
+Create your **config**.py  file with the following format:  
+```python
+# info can be found at https://developer.twitter.com/en/apps/{yourAppId} under "Keys and tokens"
+TWITTER_CONFIG = {
+    "consumer_key": "",
+    "consumer_secret": "",
+    "access_token": "",
+    "access_token_secret": "",
+}
+
+# info can be found at https://www.twilio.com/console
+TWILIO_CONFIG = {
+    "account_sid": "", 
+    "auth_token": "", 
+    "sender": "" # your twilio phone number
+}
+
+# remember to include country code (1) before phone numbers && include alternate spellings of any names! 
+CONTACTS = {
+    "Meghan": "+11234567891", 
+    "Megan": "+11234567891", 
+    "Meg": "+11234567891", 
+    "Thomas": "+17891234567", 
+    "Tom": "+17891234567",
+}
+```
+
+## Run 
+```shell 
+$ python3 pizza.py
+```
+
+## Built With 
+[Tweepy](https://www.tweepy.org/) 
+
+[Twilio](https://www.twilio.com/) 
+
+## Author 
+* **Meghan Hannon** - software developer - [github](https://github.com/megfh)
+
+## N.B. 
+* Inspiration: I missed my name day last time because I forgot to check twitter, and I don't like missing out on free pizza 🍕
+* I have this script set to run on a cron schedule on my raspberry pi, but the AWS lambda free tier would be another viable option for running on a defined schedule 
+* It may not be pretty, but it works 😊
+
+
+
+
---
+++ pizza.py
@@ -0,0 +1,64 @@
+import config
+import tweepy
+from twilio.rest import Client
+import re
+from datetime import datetime
+
+# setup 
+consumer_key = config.TWITTER_CONFIG['consumer_key']
+consumer_secret = config.TWITTER_CONFIG['consumer_secret']
+access_token = config.TWITTER_CONFIG['access_token']
+access_token_secret = config.TWITTER_CONFIG['access_token_secret']
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+twitterAPI = tweepy.API(auth)
+contacts = config.CONTACTS
+
+# returns tweet containing today's free names, if it exists 
+def getFreeNamesTweet(tweets): 
+    today = datetime.today().date()
+    for tweet in tweets: 
+        date_of_tweet = tweet.created_at.date()
+        x = re.search(".*free.*name", tweet.text)
+        if (x is not None and (today == date_of_tweet)): 
+            # the free names for the day have been tweeted! 
+            return tweet
+
+    # if the free name tweet has not been posted yet, return false 
+    return False 
+
+# returns the lucky soul who is getting free pizza on this fateful day 
+def matchNames(tweet): 
+    text = tweet.text
+    for name in contacts.keys():
+        if name in text: 
+            # woohoo free pizza, baby! 
+            return ({ "name": name, "number": contacts[name] })
+    # no free pizza for you :( 
+    return False
+
+# free pizza notification 
+def sendText(lucky): 
+    account_sid = config.TWILIO_CONFIG['account_sid']
+    auth_token  = config.TWILIO_CONFIG['auth_token']
+
+    client = Client(account_sid, auth_token)
+    text = "Yay {}! You get free pizza today :)".format(lucky['name'])
+    message = client.messages.create(
+        to=lucky['number'], 
+        from_=config.TWILIO_CONFIG['sender'],
+        body=text)
+
+
+def main(): 
+    # get latest tweets from stobies and locate today's free pizza names 
+    stobies_tweets = twitterAPI.user_timeline("@Stobiespizza")
+    tweet = getFreeNamesTweet(stobies_tweets)
+    if tweet: 
+        free = matchNames(tweet)
+        if free: 
+            # somebody gets free pizza today! let them know :) 
+            sendText(free)
+
+
+main()
